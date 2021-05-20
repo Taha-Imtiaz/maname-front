7fe4fcc8bf694e290cd1dc13193f06c7d9d53615
Taha-Imtiaz <tahaimtiaz1996@gmail.com>
2021-05-20 17:53:16 +0500

    final maname code

--- src/Pages/LoginForm/LoginForm.js
+++ src/Pages/LoginForm/LoginForm.js
@@ -1,27 +1,24 @@
-import React from 'react'
-import LoginPageHead from '../../components/LoginPageHead/LoginPageHead'
-import LoginFormFields from '../../components/LoginFormFields/LoginFormFields'
-import LoginFormButton from '../../components/LoginFormButton/LoginFormButton'
-import FormIcons from '../../components/FormIcons/FormIcons'
-import "./LoginForm.css"
-import AccountNotExists from '../../components/AccountNotExists/AccountNotExists'
-
+import React from "react";
+import LoginPageHead from "../../components/LoginPageHead/LoginPageHead";
+import LoginFormFields from "../../components/LoginFormFields/LoginFormFields";
+import LoginFormButton from "../../components/LoginFormButton/LoginFormButton";
+import FormIcons from "../../components/FormIcons/FormIcons";
+import "./LoginForm.css";
+import AccountNotExists from "../../components/AccountNotExists/AccountNotExists";
 
 const LoginForm = () => {
- 
-    return (
+  return (
+    <div className="login-form-container">
+      <div className="loginimage"></div>
+      <div className="form-components">
+        <LoginPageHead />
+        <LoginFormFields />
+        {/* <LoginFormButton/> */}
+        {/* <FormIcons/> */}
+        <AccountNotExists />
+      </div>
+    </div>
+  );
+};
 
-        <div className = "login-form-container">
-            <div className="loginimage"></div>
-            <div className="form-components">
-           <LoginPageHead/> 
-           <LoginFormFields/>
-           {/* <LoginFormButton/> */}
-           {/* <FormIcons/> */}
-           <AccountNotExists/>
-           </div>
-        </div>
-    )
-}
-
-export default LoginForm
+export default LoginForm;
